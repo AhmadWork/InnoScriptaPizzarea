3903e0f52c3139a2c5c32c6e7c24ef892dda16c8
AhmadWork <a7madswe@gmail.com>
2019-11-10 22:37:06 +0300

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,15 +10,9 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-header('Access-Control-Allow-Origin : *');
-header('Vary: Origin');
-
-if (PHP_SAPI != 'cli' && $_SERVER['REQUEST_METHOD']=='OPTIONS') {
-    header('Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, DELETE, HEAD');
-    header('Allow: POST, GET, OPTIONS, PUT, DELETE, HEAD');
-    header('Access-Control-Allow-Headers : X-Requested-With, Content-Type');
-    exit();
-}
+ header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: *'); 
+header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept');
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
